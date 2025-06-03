797805750e18184fe4b818a85c68f866ab5ce11c
DESKTOP-G2B3VNV\OWNER-S <sohee00314@gmail.com>
2025-06-04 01:43:45 +0900

    아직 엑셜과 연결하지 않음

---
+++ exceltable/src/main/java/com/sinse/exceltable/DataModel.java
@@ -0,0 +1,38 @@
+package com.sinse.exceltable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.table.AbstractTableModel;
+
+public class DataModel extends AbstractTableModel{
+	
+	Map<Integer,List> data = new HashMap<>(); // Integer는 행의 번호
+	List<String> title = new ArrayList<>();// 컬럽내용
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return data.size();
+	}
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return title.size();
+	}
+	@Override
+	public String getColumnName(int column) {
+		// TODO Auto-generated method stub
+		return title.get(column);
+	}
+
+
+	@Override
+	public Object getValueAt(int rowIndex, int colIndex) {
+		List row = data.get(rowIndex);
+		return row.get(colIndex);
+	}
+
+}
